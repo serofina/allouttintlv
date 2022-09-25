a33763ce75a7dfaaab4ea44b16392033e7de7a57
serofina.lee@gmail.com <you@example.com>
2022-09-24 17:27:19 -0700

    fixed bug on the project seletion

--- public/js/main.js
+++ public/js/main.js
@@ -87,17 +87,17 @@
   });
 
   // Portfolio isotope and filter
-  var portfolioIsotope = $(".portfolio-container").isotope({
-    itemSelector: ".portfolio-item",
-    layoutMode: "fitRows",
-  });
+  // var portfolioIsotope = $(".portfolio-container").isotope({
+  //   itemSelector: ".portfolio-item",
+  //   layoutMode: "fitRows",
+  // });
 
-  $("#portfolio-flters li").on("click", function () {
-    $("#portfolio-flters li").removeClass("active");
-    $(this).addClass("active");
+  // $("#portfolio-flters li").on("click", function () {
+  //   $("#portfolio-flters li").removeClass("active");
+  //   $(this).addClass("active");
 
-    portfolioIsotope.isotope({ filter: $(this).data("filter") });
-  });
+  //   portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  // });
 
   // Team carousel
   $(".team-carousel").owlCarousel({
