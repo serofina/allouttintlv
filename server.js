53a83b9a69ad4e6236ff0b06af6bb5f474608172
bklcross <bklcross@gmail.com>
2022-10-07 13:35:15 -0700

    fix server

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ require("dotenv").config();
 
 const cors = require("cors");
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
 app.use(
   cors({
     origin: ["https://allouttintlv.com/", "http://127.0.0.1:5500"],
   })
 );
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
